fbcef3be734d6d4e096399f667f9ed6bbf988dca
Isaac Cilia Attard <iciliaat@gmail.com>
2023-05-26 09:40:53 +0200

    Added navbar and fixed responsiveness
    Added mobile-optimised navbar, and fixed responsiveness issues across multiple different viewports.

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ const inter = Inter({
 
 const Hero = () => {
   return (
-    <div className="mx-auto my-auto flex flex-col justify-center h-screen p-4 sm:p-8 animate-pulse">
-      <div id='hero_header' className='my-3'>
+    <div className="animate-pulse h-[73%] sm:h-[90%] md:h-[90%] xl:h-[83%] 2xl:h-[90%] flex flex-col justify-center items-center text-center">
+      <div className='py-2 sm:py-3'>
         <h1 className={`text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold text-gradient bg-gradient-to-r from-[#F3F2FD] via-[#7466EC] to-[#5EBCD4] ${inter.className}`}>Isaac Cilia Attard</h1>
       </div>
-      <div id='hero_desc' className='my-3'>
-        <p className={`h-[8rem] sm:h-[4rem] text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-gradient bg-gradient-to-r from-[#F3F2FD] via-[#7466EC] to-[#5EBCD4] ${inter.className}`}>Full-stack development. AI. Quantum Computing</p>
+      <div className='py-2 sm:py-3'>
+        <p className={`text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-gradient bg-gradient-to-r from-[#F3F2FD] via-[#7466EC] to-[#5EBCD4] ${inter.className}`}>Full-stack development. AI. Quantum Computing</p>
       </div>
     </div>
   );
