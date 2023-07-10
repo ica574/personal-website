6f23a07f3d5e84de67f7ac42ee949905678e18d9
Isaac Cilia Attard <iciliaat@gmail.com>
2023-07-10 15:49:52 +0200

    Removed fonts
    Removed unnecessary fonts.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import { Inter } from 'next/font/google';
 import React from 'react';
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: '600'
-});
 
 const Hero = () => {
   return (
