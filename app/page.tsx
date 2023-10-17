ad74631c00ae2df0b8f143ad96e3ed6f0667415c
Isaac Cilia Attard <iciliaat@gmail.com>
2023-10-17 12:49:45 +0200

    Changed tagline
    Updated website tagline.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='flex flex-col justify-center min-h-screen'>
+    <div className="flex flex-col justify-center min-h-screen">
       <div className="text-center animate-pulse text-gradient bg-gradient-to-r from-[#F3F2FD] via-[#7466EC] to-[#5EBCD4]">
-        <div className='p-1'>
-          <h1 className={`text-4xl sm:text-6xl xl:text-8xl font-bold`}>Isaac Cilia Attard</h1>
+        <div className="p-1">
+          <h1 className={`text-4xl sm:text-6xl xl:text-8xl font-bold`}>
+            Isaac Cilia Attard
+          </h1>
         </div>
-        <div className='p-1'>
-          <p className={`text-lg sm:text-2xl md:text-3xl xl:text-6xl font-bold`}>Full-stack development. AI. Quantum Computing</p>
+        <div className="p-1">
+          <p
+            className={`text-lg sm:text-2xl md:text-3xl xl:text-6xl font-bold`}
+          >
+            Computer Science
+          </p>
         </div>
       </div>
     </div>
   );
 };
 
-
-export default Hero
+export default Hero;
